2087306333d743e06647afe3cbc4136dfa4ac6ff
Chantha Voeun <cvoeun45@gmail.com>
2023-03-18 22:17:30 -0400

    Arthimetic Lab Done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
+const num1 = 31;
+const num2 = 2;
 // Write your code here
+const multiply = num1*num2;
+    
+ const random = Math.floor(Math.random()*10)+ 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3%num4;
+
+const setNums = [1,2,20];
+
+const max = Math.max(...setNums);
